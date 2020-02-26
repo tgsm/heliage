454744eb0d079de3746121a1b75afa8406f8e45f
tgsm <doodrabbit@hotmail.com>
2020-02-25 22:01:00 -0500

    logging: make header-only
    at the moment, I'm only using this for fancy colors. I may bring
    back the cpp file if I want to expand logging (for example, sending log
    output to a file).

--- src/logging.h
+++ src/logging.h
@@ -1,30 +1,14 @@
 #pragma once
 
-#include <string>
+#define TEAL "\033[36m"
+#define WHITE "\033[1;37m"
+#define YELLOW "\033[1;33m"
+#define RED "\033[1;31m"
+#define PURPLE "\033[1;35m"
 
-enum class Level {
-    Trace,
-    Debug,
-    Info,
-    Warning,
-    Error,
-    Fatal,
-    LevelCount, // number of levels
-};
-
-class Logger {
-public:
-    void Log(Level level, const char* format, ...);
-
-    std::string Format(const char* format, va_list args);
-    std::string Format(const char* format, ...);
-};
-
-extern Logger logger;
-
-#define LTRACE(format, ...) logger.Log(Level::Trace, format, ##__VA_ARGS__)
-#define LDEBUG(format, ...) logger.Log(Level::Debug, format, ##__VA_ARGS__)
-#define LINFO(format, ...) logger.Log(Level::Info, format, ##__VA_ARGS__)
-#define LWARN(format, ...) logger.Log(Level::Warning, format, ##__VA_ARGS__)
-#define LERROR(format, ...) logger.Log(Level::Error, format, ##__VA_ARGS__)
-#define LFATAL(format, ...) logger.Log(Level::Fatal, format, ##__VA_ARGS__)
+#define LTRACE(format, ...) printf("trace: " format "\033[0m\n", ##__VA_ARGS__)
+#define LDEBUG(format, ...) printf(TEAL "debug: " format "\033[0m\n", ##__VA_ARGS__)
+#define LINFO(format, ...) printf(WHITE "info: " format "\033[0m\n", ##__VA_ARGS__)
+#define LWARN(format, ...) printf(YELLOW "warning: " format "\033[0m\n", ##__VA_ARGS__)
+#define LERROR(format, ...) printf(RED "error: " format "\033[0m\n", ##__VA_ARGS__)
+#define LFATAL(format, ...) printf(PURPLE "fatal: " format "\033[0m\n", ##__VA_ARGS__)
